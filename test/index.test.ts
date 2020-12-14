0f822da62f2d16fff6b4dbcea8b305b0c76d2ef2
cheap-glitch <cheap.glitch@gmail.com>
2020-12-14 10:28:04 +0100

    🚩 test: Improve tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,47 +1,45 @@
 import { whyIsNodeStillRunning } from '../src/index';
 
 import { createServer } from 'net';
-import { fetch, Response } from 'fetch-h2';
+import { fetch } from 'fetch-h2';
 
 import { Logger } from './logger';
 
-const server = createServer();
-let response: Response;
-
-// Cleanup the open resources
-afterAll(async () => {
-	await server.close();
-	await response.text();
-});
-
-test("log open handles #1", () => { // {{{
+test("log open handles #1", async () => { // {{{
 
 	// Setup
-	server.listen(0);
 	const logger = new Logger();
+	const server = createServer();
+	server.listen(0);
 
 	// Action
 	whyIsNodeStillRunning(logger);
 
 	// Tests
-	expect(logger.getLines().shift()).toMatch(/There are \d+ handle\(s\) keeping the process running/);
+	expect(logger.getLines().shift()).toMatch(/There are [3-9]\d* handle\(s\) keeping the process running/);
 	expect(logger.getOutput()).toMatch('TCPSERVERWRAP');
 	expect(logger.getOutput()).toMatch('TickObject');
+
+	// Cleanup
+	await server.close();
 
 }); // }}}
 
 test("log open handles #2", async () => { // {{{
 
 	// Setup
-	response = await fetch('https://www.google.com');
-	const logger = new Logger();
+	const logger  = new Logger();
+	const request = fetch('https://www.google.com');
 
 	// Action
 	whyIsNodeStillRunning(logger);
 
 	// Tests
-	expect(logger.getLines().shift()).toMatch(/There are \d+ handle\(s\) keeping the process running/);
+	expect(logger.getLines().shift()).toMatch(/There are [3-9]\d* handle\(s\) keeping the process running/);
 	expect(logger.getOutput()).toMatch('new Promise');
 	expect(logger.getOutput()).toMatch('why-is-node-still-running');
+
+	// Cleanup
+	await (await request).text();
 
 }); // }}}
